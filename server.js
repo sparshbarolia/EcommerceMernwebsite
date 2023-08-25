52a8d533ee8d0188563f147f11b470c2ffdc79e3
sparshbarolia <107509768+sparshbarolia@users.noreply.github.com>
2023-08-26 02:03:27 +0530

    updated

--- server.js
+++ server.js
@@ -9,12 +9,17 @@ import categoryRoutes from './routes/categoryRoutes.js';
 import productRoutes from "./routes/productRoutes.js"
 import cors from "cors";
 import path from 'path';
+import {fileURLToPath} from 'url';
 
 //configure env
 dotenv.config();
 
 //Database config
 connectDB();
+
+//esmodule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
